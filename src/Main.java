3e5d8a3ea9492bfb1297faa9a9dd7cb780203209
dmytromikulin <dimann110399@gmail.com>
2024-11-01 15:24:51 +0100

    .

--- src/Main.java
+++ src/Main.java
@@ -1,90 +1,105 @@
 import java.util.Scanner;
 
-public class App {
+class App {
+
+    private static final char[] BOX = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+    private static final int BOARD_SIZE = 9;
+    private static final char PLAYER_MARK = 'X';
+    private static final char COMPUTER_MARK = 'O';
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        byte input;
-        byte rand;
-        byte i;
-        boolean boxAvailable = false;
-        byte winner = 0;
-        char box[] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
         System.out.println("Enter box number to select. Enjoy!\n");
 
-        boolean boxEmpty = false;
         while (true) {
-            System.out.println("\n\n " + box[0] + " | " + box[1] + " | " + box[2] + " ");
-            System.out.println("-----------");
-            System.out.println(" " + box[3] + " | " + box[4] + " | " + box[5] + " ");
-            System.out.println("-----------");
-            System.out.println(" " + box[6] + " | " + box[7] + " | " + box[8] + " \n");
-            if(!boxEmpty){
-                for(i = 0; i < 9; i++)
-                    box[i] = ' ';
-                boxEmpty = true;
-            }
+            displayBoard();
 
-            if(winner == 1){
-                System.out.println("You won the game!\nCreated by Shreyas Saha. Thanks for playing!");
-                break;
-            } else if(winner == 2){
-                System.out.println("You lost the game!\nCreated by Shreyas Saha. Thanks for playing!");
-                break;
-            } else if(winner == 3){
-                System.out.println("It's a draw!\nCreated by Shreyas Saha. Thanks for playing!");
+            int gameStatus = checkGameStatus();
+            if (gameStatus != 0) {
+                displayResult(gameStatus);
                 break;
             }
 
-            while (true) {
-                input = scan.nextByte();
-                if (input > 0 && input < 10) {
-                    if (box[input - 1] == 'X' || box[input - 1] == 'O')
-                        System.out.println("That one is already in use. Enter another.");
-                    else {
-                        box[input - 1] = 'X';
-                        break;
-                    }
-                }
-                else
-                    System.out.println("Invalid input. Enter again.");
+            makePlayerMove();
+            gameStatus = checkGameStatus();
+            if (gameStatus != 0) {
+                displayBoard();
+                displayResult(gameStatus);
+                break;
             }
 
-            if((box[0]=='X' && box[1]=='X' && box[2]=='X') || (box[3]=='X' && box[4]=='X' && box[5]=='X') || (box[6]=='X' && box[7]=='X' && box[8]=='X') ||
-                    (box[0]=='X' && box[3]=='X' && box[6]=='X') || (box[1]=='X' && box[4]=='X' && box[7]=='X') || (box[2]=='X' && box[5]=='X' && box[8]=='X') ||
-                    (box[0]=='X' && box[4]=='X' && box[8]=='X') || (box[2]=='X' && box[4]=='X' && box[6]=='X')){
-                winner = 1;
-                continue;
-            }
+            makeComputerMove();
+        }
 
-            boxAvailable = false;
-            for(i=0; i<9; i++){
-                if(box[i] != 'X' && box[i] != 'O'){
-                    boxAvailable = true;
-                    break;
-                }
-            }
+        SCANNER.close();     // Закриття Scanner додає правильне управління ресурсами для запобігання потенційних витоків
+    }
 
-            if(boxAvailable == false){
-                winner = 3;
-                continue;
-            }
+    private static void displayBoard() {
+        System.out.println("\n\n " + BOX[0] + " | " + BOX[1] + " | " + BOX[2]);
+        System.out.println("-----------");
+        System.out.println(" " + BOX[3] + " | " + BOX[4] + " | " + BOX[5]);
+        System.out.println("-----------");
+        System.out.println(" " + BOX[6] + " | " + BOX[7] + " | " + BOX[8] + " \n");
+    }
 
-            while (true) {
-                rand = (byte) (Math.random() * (9 - 1 + 1) + 1);
-                if (box[rand - 1] != 'X' && box[rand - 1] != 'O') {
-                    box[rand - 1] = 'O';
-                    break;
-                }
-            }
+    private static int checkGameStatus() {       // зберігає всі можливі виграшні комбінації і перевіряє,
+                                                 // чи є переможець. Це усуває дублювання та підвищує читабельність
+        int[][] winningCombinations = {
+                {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // Рядки
+                {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // Стовбці
+                {0, 4, 8}, {2, 4, 6}             // Діагоналі
+        };
 
-            if((box[0]=='O' && box[1]=='O' && box[2]=='O') || (box[3]=='O' && box[4]=='O' && box[5]=='O') || (box[6]=='O' && box[7]=='O' && box[8]=='O') ||
-                    (box[0]=='O' && box[3]=='O' && box[6]=='O') || (box[1]=='O' && box[4]=='O' && box[7]=='O') || (box[2]=='O' && box[5]=='O' && box[8]=='O') ||
-                    (box[0]=='O' && box[4]=='O' && box[8]=='O') || (box[2]=='O' && box[4]=='O' && box[6]=='O')){
-                winner = 2;
-                continue;
+        for (int[] combo : winningCombinations) {
+            if (BOX[combo[0]] == PLAYER_MARK && BOX[combo[1]] == PLAYER_MARK && BOX[combo[2]] == PLAYER_MARK) {
+                return 1; // Гравець одержує перемогу
+            }
+            if (BOX[combo[0]] == COMPUTER_MARK && BOX[combo[1]] == COMPUTER_MARK && BOX[combo[2]] == COMPUTER_MARK) {
+                return 2; // Комп'ютер одержує перемогу
             }
         }
 
+        for (char c : BOX) {
+            if (c != PLAYER_MARK && c != COMPUTER_MARK) {
+                return 0; // Гра ще триває
+            }
+        }
+
+        return 3; // Нічия
+    }
+
+    private static void makePlayerMove() {             // обробляє хід гравця з перевіркою введення
+        while (true) {
+            System.out.print("Enter your move (1-9): ");
+            byte input = SCANNER.nextByte();
+            if (input > 0 && input <= BOARD_SIZE && isBoxFree(input - 1)) {
+                BOX[input - 1] = PLAYER_MARK;
+                break;
+            }
+            System.out.println("Invalid input or box already taken. Please try again.");
+        }
+    }
+
+    private static void makeComputerMove() {         // обробляє хід комп'ютера з випадковим вибором вільного поля
+        while (true) {
+            int rand = (int) (Math.random() * BOARD_SIZE);
+            if (isBoxFree(rand)) {
+                BOX[rand] = COMPUTER_MARK;
+                break;
+            }
+        }
+    }
+
+    private static boolean isBoxFree(int index) {
+        return BOX[index] != PLAYER_MARK && BOX[index] != COMPUTER_MARK;
+    }
+
+    private static void displayResult(int gameStatus) {
+        displayBoard();
+        switch (gameStatus) {
+            case 1 -> System.out.println("You won the game!\nThanks for playing!");
+            case 2 -> System.out.println("You lost the game!\nThanks for playing!");
+            case 3 -> System.out.println("It's a draw!\nThanks for playing!");
+        }
     }
 }
